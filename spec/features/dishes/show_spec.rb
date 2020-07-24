5f97276d42aec6646b2556256cb262c5d4d5e8de
Dan Sehl <dan.sehl@gmail.com>
2020-07-24 09:45:54 -0600

    Add spec and functionality to show a sum of all calories for a dish

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -19,4 +19,17 @@ RSpec.describe 'dish show page', type: :feature do
     expect(page).to have_content(ingredient_2.calories)
     expect(page).to have_content(chef_1.name)
   end
+
+  it "totals the calorie count for all ingredients in a dish" do
+    chef_1 = Chef.create!(name: "Gordon Ramsay")
+    dish_1 = chef_1.dishes.create!(name: "Beef Wellington", description: "Amazing")
+    ingredient_1 = Ingredient.create!(name: "Wild Mushrooms", calories: 100)
+    ingredient_2 = Ingredient.create!(name: "Beef Fillet", calories: 500)
+    DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_1.id)
+    DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_2.id)
+
+    visit "dishes/#{dish_1.id}"
+
+    expect(page).to have_content("Total Calories: 600")
+  end
 end
