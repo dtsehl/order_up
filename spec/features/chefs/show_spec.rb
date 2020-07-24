71d5d46ab211795fa9b276e9be72402cb477d203
Dan Sehl <dan.sehl@gmail.com>
2020-07-24 10:54:52 -0600

    Add extension test for most popular chef dishes

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -25,4 +25,32 @@ RSpec.describe 'chef show page', type: :feature do
     expect(page).to have_content(ingredient_1.name)
     expect(page).to have_content(ingredient_2.name)
   end
+
+  it "displays the chef's three most popular ingredients" do
+    chef_1 = Chef.create!(name: "Gordon Ramsay")
+    dish_1 = chef_1.dishes.create!(name: "Beef Wellington", description: "Amazing")
+    dish_2 = chef_1.dishes.create!(name: "Omlette", description: "Stupendous")
+    dish_3 = chef_1.dishes.create!(name: "Baked Salmon", description: "Spectacular")
+    ingredient_1 = Ingredient.create!(name: "Wild Mushrooms", calories: 100)
+    ingredient_2 = Ingredient.create!(name: "Beef Fillet", calories: 500)
+    ingredient_3 = Ingredient.create!(name: "Eggs", calories: 200)
+    ingredient_4 = Ingredient.create!(name: "Cheese", calories: 150)
+    ingredient_5 = Ingredient.create!(name: "Salmon", calories: 400)
+    DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_1.id)
+    DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_2.id)
+    DishIngredient.create!(dish_id: dish_2.id, ingredient_id: ingredient_3.id)
+    DishIngredient.create!(dish_id: dish_2.id, ingredient_id: ingredient_4.id)
+    DishIngredient.create!(dish_id: dish_2.id, ingredient_id: ingredient_1.id)
+    DishIngredient.create!(dish_id: dish_2.id, ingredient_id: ingredient_2.id)
+    DishIngredient.create!(dish_id: dish_3.id, ingredient_id: ingredient_4.id)
+    DishIngredient.create!(dish_id: dish_3.id, ingredient_id: ingredient_1.id)
+    DishIngredient.create!(dish_id: dish_3.id, ingredient_id: ingredient_5.id)
+
+    visit "chefs/#{chef_1.id}"
+
+    expect(page).to have_content("Three most popular ingredients:")
+    expect(page).to have_content(ingredient_1.name)
+    expect(page).to have_content(ingredient_2.name)
+    expect(page).to have_content(ingredient_4.name)
+  end
 end
