39d0c7be50fbbc3f4a1d7a961c0a015d55a1e522
Dan Sehl <dan.sehl@gmail.com>
2020-07-24 10:55:38 -0600

    Attempted to write the most_popular_ingredients method

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,8 @@ class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+  def most_popular_ingredients
+    # I think I was on the right track here, but need to change my ActiveRecord query
+    @ingredients = Ingredient.select("name").joins(:dish_ingredients).group(:ingredient_id).order("ingredient_id desc").limit(3)
+  end
 end
